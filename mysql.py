4add5f6d16d2f8daf175c0939afce68fd1f1bf35
spring19940512 <794255955@qq.com>
2017-04-08 16:17:09 +0800

    test成功

--- mysql.py
+++ mysql.py
@@ -7,3 +7,19 @@
 
 import os
 import MySQLdb
+# 打开数据库连接
+db = MySQLdb.connect("107.191.118.80","root","123456","twitter3" )
+
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+
+# 使用execute方法执行SQL语句
+cursor.execute("SELECT VERSION()")
+
+# 使用 fetchone() 方法获取一条数据库。
+data = cursor.fetchone()
+
+print "Database version : %s " % data
+
+# 关闭数据库连接
+db.close()
