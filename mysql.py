967a5b54058ec24e1b7bf77047e07f7bf74162f6
spring19940512 <794255955@qq.com>
2017-04-08 16:11:55 +0800

    mysql.py

---
+++ mysql.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-08 16:08:57
+# @Author  : mx (spring_chu@sjtu.edu.cn)
+# @Link    : ${link}
+# @Version : $Id$
+
+import os
+import MySQLdb
